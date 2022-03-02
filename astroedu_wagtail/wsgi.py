d28f6830f8713914438b6823c22aa344dd379323
Edward Gomez <edward@gomez.me.uk>
2022-03-02 16:38:01 +0000

    Use prod in wsgi

--- astroedu_wagtail/wsgi.py
+++ astroedu_wagtail/wsgi.py
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "astroedu_wagtail.settings.dev")
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "astroedu_wagtail.settings.production")
 
 application = get_wsgi_application()
